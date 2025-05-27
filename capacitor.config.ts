c15be3d81f805daad23b164566dd9f5497b2e372
Lluvia <llxbxzxm@gmail.com>
2025-05-27 09:41:12 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'APPITI22Tab',
+  webDir: 'www'
+};
+
+export default config;
